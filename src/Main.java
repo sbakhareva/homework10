b8c3cf038dc77161ab01589fc62364f8def53c5f
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-10-08 18:42:42 +0300

    готовое дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        // Task #1
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName  + " " + firstName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
+
+        // Task #2
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName);
+
+        //Task #3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
